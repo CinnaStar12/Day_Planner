75f8cb2031619c71cbc6ac82c157ef020830f681
Allan Binayao <allan.binayao@gmail.com>
2020-01-29 17:59:45 -0600

    html and js functional complete

--- script.js
+++ script.js
@@ -4,28 +4,56 @@ var tasklist = [];
 
 $("document").ready(function () {
     dateLine.text(moment().format("MMMM Do YYYY"));
+    pullTaskList();
     console.log(now);
-    $(".text-block-area").each(function() {
-        if(now > $(this).attr("data-time")){
+    console.log(tasklist);
+    $(".text-block-area").each(function () {
+        if (now > $(this).attr("data-time")) {
             $(this).attr("class", "form-control text-block-area text-block-before");
         }
-        else if(now == $(this).attr("data-time")){
+        else if (now == $(this).attr("data-time")) {
             $(this).attr("class", "form-control text-block-area text-block-during");
         }
-        else if(now < $(this).attr("data-time")){
+        else if (now < $(this).attr("data-time")) {
             $(this).attr("class", "form-control text-block-area text-block-after");
         }
     });
-    
-    
+
+
 });
-$(".time-button").on("click", function() {
+$(".time-button").on("click", function () {
     var timeVal = $(this).attr("data-time")
+    var taskInput = $("textarea[data-time=" + timeVal + "]").val();
+    var index = (timeVal - 9);
+    tasklist[index] = taskInput;
+    var tasklistStr = JSON.stringify(tasklist);
+    localStorage.setItem("tasklist", tasklistStr)
     console.log(timeVal);
-    var task = $("textarea[data-time=" + timeVal + "]").val();
-    console.log(task);
-    tasklist.push({
-        time: timeVal,
-        task: task
-    })
+    console.log(taskInput);
+    console.log(tasklist)
+    console.log(tasklist.length)
 })
+
+function pullTaskList() {
+    var tasklistStr = localStorage.getItem("tasklist");
+    tasklist = JSON.parse(tasklistStr)
+    $(".text-block-area").each(function () {
+        var timeVal = $(this).attr("data-time");
+        var index = (timeVal - 9);
+        $(this).text(tasklist[index])
+
+    })
+};
+
+$(".btn-danger").on("click", function () {
+    var check = confirm(    "Are you sure you want to delete all tasks? All data will be erased and will not be able to be recovered.")
+    if (check) {
+        $(".text-block-area").each(function () {
+            $(this).val("")
+        })
+        tasklist = [""];
+        console.log(tasklist)
+        var tasklistStr = JSON.stringify(tasklist);
+        localStorage.setItem("tasklist", tasklistStr)
+    }
+});
